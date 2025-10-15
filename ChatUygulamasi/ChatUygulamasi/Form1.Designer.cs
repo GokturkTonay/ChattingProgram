9a5e170f6746d25f1452cfca1af8479503860a63
GöktürkTonay <gokturktonay@gmail.com>
2025-10-15 16:42:29 +0300

    Update

---
+++ ChatUygulamasi/ChatUygulamasi/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ChatUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gboxGiris = new System.Windows.Forms.GroupBox();
+            this.lblKullaniciAdi = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.lstMesajlar = new System.Windows.Forms.ListBox();
+            this.gboxSohbet = new System.Windows.Forms.GroupBox();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.gboxGiris.SuspendLayout();
+            this.gboxSohbet.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gboxGiris
+            // 
+            this.gboxGiris.Controls.Add(this.lblKullaniciAdi);
+            this.gboxGiris.Controls.Add(this.txtKullaniciAdi);
+            this.gboxGiris.Controls.Add(this.btnBaglan);
+            this.gboxGiris.Location = new System.Drawing.Point(42, 31);
+            this.gboxGiris.Name = "gboxGiris";
+            this.gboxGiris.Size = new System.Drawing.Size(385, 153);
+            this.gboxGiris.TabIndex = 0;
+            this.gboxGiris.TabStop = false;
+            this.gboxGiris.Text = "Giriş";
+            // 
+            // lblKullaniciAdi
+            // 
+            this.lblKullaniciAdi.AutoSize = true;
+            this.lblKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullaniciAdi.Location = new System.Drawing.Point(12, 48);
+            this.lblKullaniciAdi.Name = "lblKullaniciAdi";
+            this.lblKullaniciAdi.Size = new System.Drawing.Size(108, 24);
+            this.lblKullaniciAdi.TabIndex = 1;
+            this.lblKullaniciAdi.Text = "KullanıcıAdı";
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(126, 45);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(135, 27);
+            this.txtKullaniciAdi.TabIndex = 2;
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaglan.Location = new System.Drawing.Point(267, 36);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(112, 44);
+            this.btnBaglan.TabIndex = 3;
+            this.btnBaglan.Text = "Bağlan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // lstMesajlar
+            // 
+            this.lstMesajlar.FormattingEnabled = true;
+            this.lstMesajlar.ItemHeight = 16;
+            this.lstMesajlar.Location = new System.Drawing.Point(6, 30);
+            this.lstMesajlar.Name = "lstMesajlar";
+            this.lstMesajlar.Size = new System.Drawing.Size(373, 116);
+            this.lstMesajlar.TabIndex = 5;
+            // 
+            // gboxSohbet
+            // 
+            this.gboxSohbet.Controls.Add(this.btnGonder);
+            this.gboxSohbet.Controls.Add(this.txtMesaj);
+            this.gboxSohbet.Controls.Add(this.lstMesajlar);
+            this.gboxSohbet.Location = new System.Drawing.Point(42, 190);
+            this.gboxSohbet.Name = "gboxSohbet";
+            this.gboxSohbet.Size = new System.Drawing.Size(385, 233);
+            this.gboxSohbet.TabIndex = 7;
+            this.gboxSohbet.TabStop = false;
+            this.gboxSohbet.Text = "Sohbet";
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMesaj.Location = new System.Drawing.Point(6, 177);
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(199, 27);
+            this.txtMesaj.TabIndex = 6;
+            this.txtMesaj.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMesaj_KeyDown);
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGonder.Location = new System.Drawing.Point(211, 170);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(113, 43);
+            this.btnGonder.TabIndex = 7;
+            this.btnGonder.Text = "Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 455);
+            this.Controls.Add(this.gboxSohbet);
+            this.Controls.Add(this.gboxGiris);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gboxGiris.ResumeLayout(false);
+            this.gboxGiris.PerformLayout();
+            this.gboxSohbet.ResumeLayout(false);
+            this.gboxSohbet.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gboxGiris;
+        private System.Windows.Forms.Label lblKullaniciAdi;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.ListBox lstMesajlar;
+        private System.Windows.Forms.GroupBox gboxSohbet;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.TextBox txtMesaj;
+    }
+}
+
---
+++ ChatUygulamasi/ChatUygulamasi/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udpListener;
+        private const int Port = 15000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtKullaniciAdi.Text))
+            {
+                MessageBox.Show("Lütfen bir kullanıcı adı girin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // ... (UdpClient ayarları aynı kalacak, port paylaşımını destekleyen kod) ...
+                udpListener = new UdpClient();
+                udpListener.ExclusiveAddressUse = false;
+                udpListener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, Port);
+                udpListener.Client.Bind(localEndPoint);
+
+                // Dinlemeyi başlat
+                Task.Run(() => ListenForMessages());
+
+                // ---- YENİ KOD ----
+                // Herkese katıldığınızı bildiren bir mesaj yayınlayın
+                string joinMessage = $"[{txtKullaniciAdi.Text} sohbete katıldı.]";
+                byte[] data = Encoding.UTF8.GetBytes(joinMessage);
+                using (UdpClient udpSender = new UdpClient())
+                {
+                    IPEndPoint broadcastEndpoint = new IPEndPoint(IPAddress.Broadcast, Port);
+                    udpSender.Send(data, data.Length, broadcastEndpoint);
+                }
+                // ---- YENİ KOD BİTİŞİ ----
+
+                // Arayüzü güncelle
+                gboxGiris.Enabled = false;
+                gboxSohbet.Enabled = true;
+                txtMesaj.Focus();
+                this.Text = $"Lan Chat - {txtKullaniciAdi.Text}";
+                lstMesajlar.Items.Add("[Sohbete hoş geldiniz...]");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private async Task ListenForMessages()
+        {
+            try
+            {
+                while (true)
+                {
+                    UdpReceiveResult result = await udpListener.ReceiveAsync();
+                    byte[] buffer = result.Buffer;
+                    string message = Encoding.UTF8.GetString(buffer);
+
+                    // Gelen mesajı gönderen biz miyiz?
+                    // Eğer mesaj bizim kullanıcı adımızla başlıyorsa, bu bizim gönderdiğimiz
+                    // mesajın yankısıdır. Zaten "Siz: ..." olarak eklediğimiz için bunu görmezden gel.
+                    if (message.StartsWith($"{txtKullaniciAdi.Text}:"))
+                    {
+                        continue; // Döngünün bir sonraki adımına geç, bu mesajı işleme.
+                    }
+
+                    // Başkasından gelen mesajı listeye ekle.
+                    UpdateChatHistory(message);
+                }
+            }
+            catch (ObjectDisposedException) { }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mesaj alınırken hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateChatHistory(string message)
+        {
+            if (lstMesajlar.InvokeRequired)
+            {
+                // Eğer bu metot farklı bir thread'den çağrıldıysa, ana UI thread'ine yönlendir.
+                lstMesajlar.Invoke(new MethodInvoker(delegate { UpdateChatHistory(message); }));
+            }
+            else
+            {
+                // Ana UI thread'indeysek, listeye mesajı ekle.
+                lstMesajlar.Items.Add(message);
+                // Otomatik olarak en alta kaydır
+                lstMesajlar.TopIndex = lstMesajlar.Items.Count - 1;
+            }
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void SendMessage()
+        {
+            if (udpListener == null || string.IsNullOrWhiteSpace(txtMesaj.Text))
+            {
+                return;
+            }
+
+            try
+            {
+                // Mesaj formatı: "KullanıcıAdı: Mesaj"
+                string fullMessage = $"{txtKullaniciAdi.Text}: {txtMesaj.Text}";
+                byte[] data = Encoding.UTF8.GetBytes(fullMessage);
+
+                // Kendi gönderdiğimiz mesajı "Siz:" olarak kendi listemize hemen ekleyelim
+                UpdateChatHistory($"Siz: {txtMesaj.Text}");
+
+                // Mesajı ağdaki tüm cihazlara (broadcast) gönder
+                using (UdpClient udpSender = new UdpClient())
+                {
+                    IPEndPoint broadcastEndpoint = new IPEndPoint(IPAddress.Broadcast, Port);
+                    udpSender.Send(data, data.Length, broadcastEndpoint);
+                }
+
+                // Mesaj kutusunu temizle
+                txtMesaj.Clear();
+                txtMesaj.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mesaj gönderilemedi: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtMesaj_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Eğer Enter tuşuna basıldıysa ve Shift tuşuna basılmıyorsa (alt satıra geçmek için)
+            if (e.KeyCode == Keys.Enter && !e.Shift)
+            {
+                e.SuppressKeyPress = true; // Enter tuşunun kendi "bip" sesini engelle
+                SendMessage();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (udpListener != null)
+            {
+                udpListener.Close();
+                udpListener = null;
+            }
+        }
+    }
+}
